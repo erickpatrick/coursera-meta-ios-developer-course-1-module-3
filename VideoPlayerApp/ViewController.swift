9f59a511b9c0a5182ba464ce252ef5e1e2a7d7f0
erick patrick <github@erickpatrick.net>
2024-12-27 15:46:33 +0100

    Initial Commit

---
+++ VideoPlayerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayerApp
+//
+//  Created by Erick Rocha on 27.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
